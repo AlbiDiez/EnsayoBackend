b4c8e491dfd8c6beb0242766e6d921533ebc23f6
AlbiDiez <albidiez10@gmail.com>
2024-08-15 13:24:40 -0500

    Add Entidad Modelo

--- obj/Debug/net8.0/ProyectoBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProyectoBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1661a38c7a0c35f4331b3caf3e2d640c1dce30c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
